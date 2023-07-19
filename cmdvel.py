b039a1bf586c99a30c5e45597c0000fd01bd5c23
Sanskruti Jadhav <64002247+Autonomousanz@users.noreply.github.com>
2023-07-19 16:32:18 -0400

    Add files via upload

--- cmdvel.py
+++ cmdvel.py
@@ -12,19 +12,21 @@ def publisher():
     rate = rospy.Rate(100)
   
     vel = Twist()
-    time_0 = rospy.Time.now().to_sec()
-    time_1 = rospy.Time.now().to_sec()
-    time_diff = time_1-time_0
     i=1
     while not rospy.is_shutdown():
-        if i < 6000:
+        if i <= 1000:
+            
+            vel.linear.x = 0.5
+            vel.angular.z = 0.0  
+
+        elif 1000 < i <= 4000:
             vel.linear.x = 0.2
-            vel.angular.z = -0.00002*i
-       
+            vel.angular.z = 0.5   
 
         else:
             vel.linear.x = 0
             vel.angular.z = 0
+            
         pub.publish(vel)
         i = i+1
         print(i)
